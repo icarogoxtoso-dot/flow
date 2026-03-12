1983f4cb55422eb9d68f729080026cb5e8006898
icarogoxtoso-dot <icarogoxtoso@gmail.com>
2026-03-12 02:44:24 -0300

    Delete access directory

--- access/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../secure/auth.php';
-require_once __DIR__ . '/../secure/config.php';
-startSecureSession();
-logoutUser();
-header('Location: ' . appPath('/access/login.php?mode=login'));
-exit;
